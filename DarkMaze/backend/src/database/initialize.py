75913b357f17b2377a585e6be91a3e202dd1fb00
alexc_313 <chuangyungtai@gmail.com>
2025-03-07 01:33:56 +0800

    style: clean up code

--- DarkMaze/backend/src/database/initialize.py
+++ DarkMaze/backend/src/database/initialize.py
@@ -3,21 +3,21 @@ import sqlite3
 
 def initialize():
     conn = sqlite3.connect("game.db")
-    cursor = conn.cursor()
+    # cursor = conn.cursor()
 
-    # Create game_state table (with username column)
-    cursor.execute(
-        """
-    CREATE TABLE IF NOT EXISTS game_state (
-        id INTEGER PRIMARY KEY AUTOINCREMENT,
-        username TEXT NOT NULL,
-        current_level_name TEXT NOT NULL,
-        map_size TEXT NOT NULL,
-        health INTEGER NOT NULL,
-        path TEXT NOT NULL,
-        current_position TEXT NOT NULL
-    )
-    """
-    )
+    # # Create game_state table (with username column)
+    # cursor.execute(
+    #     """
+    # CREATE TABLE IF NOT EXISTS game_state (
+    #     id INTEGER PRIMARY KEY AUTOINCREMENT,
+    #     username TEXT NOT NULL,
+    #     current_level_name TEXT NOT NULL,
+    #     map_size TEXT NOT NULL,
+    #     health INTEGER NOT NULL,
+    #     path TEXT NOT NULL,
+    #     current_position TEXT NOT NULL
+    # )
+    # """
+    # )
     conn.commit()
     conn.close()
